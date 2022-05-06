9b341267a7938938da468019cf76fefdcc0c3dfe
markoni313 <patrykzygmuntzx@gmail.com>
2022-05-06 14:01:28 +0200

    Update environment.ts

--- Angular/HelloWorls/src/environments/environment.ts
+++ Angular/HelloWorls/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
    firebaseConfig : {
-    apiKey: "AIzaSyCL2ElBGBeVyMz9_GidafnI_lrtHZeCqgQ",
-    authDomain: "projekt-web-f196a.firebaseapp.com",
-    databaseURL: "https://projekt-web-f196a.firebaseio.com",
-    projectId: "projekt-web-f196a",
-    storageBucket: "projekt-web-f196a.appspot.com",
-    messagingSenderId: "1081550481149",
-    appId: "1:1081550481149:web:7e3720848622ef6c2a0094",
-    measurementId: "G-54TNVC9DCG"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   },
   server_url :'http://localhost:3000/'
 };
